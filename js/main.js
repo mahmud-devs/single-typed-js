acb452b6c9837e2bb85fd11d59c819b859818cf5
mahmud-devs <mahmudulislam0999@gmail.com>
2024-03-05 05:45:22 +0600

    single typed js

---
+++ js/main.js
@@ -0,0 +1,21 @@
+/*
+================ hours all properties selectors      =================
+*/
+
+const type = document.querySelector(".type");
+const nam = type.dataset.name;
+
+let typing = -1;
+
+function wirte() {
+    typing++;
+    type.innerHTML += nam.charAt(typing);
+    if (typing == nam.length) {
+        typing = -1;
+        type.innerHTML = "";
+    }
+}
+
+let stop = setInterval(() => {
+    wirte();
+}, 100);

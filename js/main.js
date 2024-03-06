97bd86d5f3176902f29e8c0c2e01228632e7203a
mahmud-devs <mahmudulislam0999@gmail.com>
2024-03-07 03:31:28 +0600

    '

--- js/main.js
+++ js/main.js
@@ -2,20 +2,40 @@
 ================ hours all properties selectors      =================
 */
 
-const type = document.querySelector(".type");
-const nam = type.dataset.name;
-
-let typing = -1;
-
-function wirte() {
-    typing++;
-    type.innerHTML += nam.charAt(typing);
-    if (typing == nam.length) {
-        typing = -1;
-        type.innerHTML = "";
-    }
+function fun1(callBackTwo) {
+    setTimeout(function () {
+        console.log(`this is function : 1`);
+        callBackTwo();
+    }, 500);
+}
+function fun2() {
+    console.log(`this is function : 2`);
 }
 
-let stop = setInterval(() => {
-    wirte();
-}, 100);
+function fun3(callBackOne) {
+    console.log(`this is function : 3`);
+    callBackOne();
+}
+
+function fun4(callBackThree) {
+    console.log(`this is function : 4`);
+    callBackThree()
+}
+
+function fun5() {
+    console.log(`this is function : 5`);
+}
+
+function fun6() {
+    console.log(`this is function : 6`);
+}
+
+fun3(function () {
+    fun1(function () {
+        fun4(function () {
+            fun2(function () {
+                
+            })
+        });
+    });
+});
